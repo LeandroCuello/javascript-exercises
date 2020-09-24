86a01d638e1586716868819b7978d7e2cf6fbe13
Leando Cuello <leandroezequiel1999@gmail.com>
2020-09-24 15:30:25 -0300

    finished remove From Array exersice

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(numberArray, ...eliminateList) {
 
+for(let i = 0; i < eliminateList.length; i++){
+    numberArray = eliminateRepeted(numberArray, eliminateList[i]);
+}
+return numberArray;
+}
+
+function eliminateRepeted(string1, subjectToElimite){
+    for(i=0; i<string1.length; i++){
+        if(string1[i] === subjectToElimite){
+            string1.splice(i, 1);
+        }
+    }
+    return string1;
 }
 
 module.exports = removeFromArray
