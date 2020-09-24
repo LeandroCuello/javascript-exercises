3e3617266319837dd21579a5eb45cc9dd2a4dc14
Leando Cuello <leandroezequiel1999@gmail.com>
2020-09-24 16:22:49 -0300

    Finish tempConversion exercise

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(faTmp) {
+let cTmp = (faTmp - 32)* (5/9);
+return Math.round(cTmp * 10)/10;
 }
 
-const ctof = function() {
-
+const ctof = function(cTmp) {
+let faTmp = cTmp * (9/5) + 32;
+return Math.round(faTmp * 10)/10;
 }
 
 module.exports = {
