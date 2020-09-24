e0ec79027827927cae3922542edb80f3edcaf51c
Leando Cuello <leandroezequiel1999@gmail.com>
2020-09-24 14:06:47 -0300

    finished reapetString exercise

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(text, nOfTimes) {
+    let finalText = "";
+if(nOfTimes <0){
+    return "ERROR";
+}
+for(let i=0;i<nOfTimes;i++){
+    finalText +=text;
+}
+return finalText;
 }
 
 module.exports = repeatString
