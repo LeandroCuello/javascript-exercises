6348253797e6d476ee5657932d5ababd859de243
Leando Cuello <leandroezequiel1999@gmail.com>
2020-09-24 15:49:19 -0300

    finish sumAll exersice

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,4 +1,25 @@
-const sumAll = function() {
+const sumAll = function(number1, number2) {
+
+if(number1 < 0 || number2 < 0 || typeof(number1) != "number" || typeof(number2) != "number"){
+    return "ERROR";
+}
+
+let biggestNumber, smallestNumber, finalNumber;
+finalNumber =0;
+
+if(number1>number2){
+    biggestNumber = number1;
+    smallestNumber = number2;
+}else{
+    biggestNumber = number2;
+    smallestNumber = number1;
+}
+
+for (let i = smallestNumber; i<=biggestNumber; i++){
+    finalNumber += i;
+}
+
+return finalNumber;
 
 }
 
